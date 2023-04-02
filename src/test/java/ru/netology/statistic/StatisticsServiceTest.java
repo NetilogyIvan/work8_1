7304faaf7a4b25d4d974efc369386e1751525895
NetilogyIvan <inkornelyuk@gmail.com>
2023-04-03 00:58:28 +0600

    add JoCoCo test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,9 +1,11 @@
 package ru.netology.statistic;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
-@Test
-    void findMax() {
+    @Test
+    void findMaxOne() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -11,6 +13,18 @@ public class StatisticsServiceTest {
 
         long actual = service.findMax(incomesInBillions);
 
-    Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxTwo() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 15, 8, 6, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
